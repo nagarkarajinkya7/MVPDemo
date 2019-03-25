a4dcb6be65007694df230d6e094aed192aa4dd96
ajinkya <nagarkarajinkya7@gmail.com>
2019-03-25 13:14:58 +0530

    User Model created

--- app/src/main/java/com/appmaker/mvpdemo/Model/User.java
+++ app/src/main/java/com/appmaker/mvpdemo/Model/User.java
@@ -1,4 +1,37 @@
 package com.appmaker.mvpdemo.Model;
 
 public class User {
+
+    private String fName = null;
+    private String eMail = null;
+
+    public User() {
+    }
+
+    public User(String fName, String eMail) {
+        this.fName = fName;
+        this.eMail = eMail;
+    }
+
+    public String getFullName() {
+        return fName;
+    }
+
+    public void setFullName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getMailId() {
+        return eMail;
+    }
+
+    public void setEmailId(String eMail) {
+        this.eMail = eMail;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Email Address : " + eMail + "\nFull Name : " + fName;
+    }
 }
